da3a16db9f8d45f0019f169d99c62141ffde96ef
Kristian Mandrup <kmandrup@gmail.com>
2010-08-26 12:28:22 +0200

    improved modules extension handling API

--- lib/rspec-action_view/rspec/macro.rb
+++ lib/rspec-action_view/rspec/macro.rb
@@ -1,24 +1,21 @@
 require 'active_support/railtie'
 require 'rails3_plugin_toolbox'
+require 'sugar-high/kind_of'
 
 module RSpec
   module ActionView
     module Macro
-      def extend_view_with_modules *modules
+      def extend_view_with base_name, *modules
+        modules = modules.flatten 
         Rails3::PluginExtender.new do
           # extend action_view with methods from some modules
-          extend_rails :view do                 
-            extend_with *modules
-          end    
-        end
-      end
-
-      def extend_view_with base_name, *modules 
-        Rails3::PluginExtender.new do
-          # extend action_view with methods from some modules
-          extend_rails :view do
-            extend_from_module base_name, *modules 
-          end    
+          extend_rails :view do |v|   
+            if base_name.kind_of?(Module) && !modules.empty? && modules.only_kinds_of?(Symbol)
+              v.extend_from_module(base_name, *modules) 
+            else
+              v.extend_with base_name, *modules
+            end
+          end
         end
       end
     end
--- spec/rspec-action_view/erb_tester_spec.rb
+++ spec/rspec-action_view/erb_tester_spec.rb
@@ -1,10 +1,27 @@
 require 'spec_helper'
 
 module MyView
+  
+  def goodbye
+    'Goodbye'
+  end
+  
   module Tab
     def tab_for(clazz, &block)               
       content = with_output_buffer(&block)  
       content_tag :li, content, :class => clazz
+    end
+  end
+
+  module Blip
+    def blip
+      content_tag :li, 'blip me'
+    end
+  end
+
+  module Blap
+    def blap
+      content_tag :li, 'blap me'
     end
   end
 
@@ -22,11 +39,38 @@ end
 
 describe 'My View extensions!' do
   extend_view_with MyView, :tab, :say
+  extend_view_with MyView
+  extend_view_with MyView::Blap, 'MyView::Blip'
   
-  it "should work" do
+  it "should extend with single module MyView" do
+    with_engine(:erb) do |e|
+      e.run_template("<%= goodbye %>").should match /Goodbye/
+    end
+  end
+
+
+  it "should extend with module Say - hello, name" do
     with_engine(:erb) do |e|
       e.run_template("hello <%= name %>").should match /Kristian/
+  
+      e.run_template do 
+        %{
+          <%= hello :blip do %>
+            ged
+          <% end %>
+        }
+      end.should match /ged/
+    end
+  end
 
+  it "should extend with modules Blip and Blap" do
+    with_engine(:erb) do |e|
+      e.run_template("hello <%= blip %> go <%= blap %>").should match /blip me/
+    end
+  end
+
+  it "should extend handle nested blocks in ERB" do
+    with_engine(:erb) do |e|
       e.run_template do 
         %{<%= tab_for :x do %>
           <%= hello :blip do %>
@@ -36,4 +80,5 @@ describe 'My View extensions!' do
       end.should match /ged/
     end
   end
+
 end
